c946ba93323526184f7f48d6338d6d2e22cd3bb6
Mikkel Jeppesen Juhl <mikkeljuhl@Mikkels-MacBook-Pro-2.local>
2016-12-28 00:13:49 +0100

    basket

--- app/Basket.php
+++ app/Basket.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Logger extends Model
+class Basket extends Model
 {
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,12 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+/* Basket */
+
+Route::get('/basket', 'BasketController@index');
+
+/* Products */
+
 Route::get('/products', 'ProductController@index');
 Route::get('/products/create', 'ProductController@create');
 Route::post('/products/store', 'ProductController@store');
@@ -28,6 +34,7 @@ Route::get('/products/{product}/edit', 'ProductController@edit');
 Route::put('/products/{product}', 'ProductController@update');
 Route::delete('/products/{product}', 'ProductController@destroy');
 
+/* Categories */
 
 Route::get('/categories', 'CategoryController@index');
 Route::get('/categories/create', 'CategoryController@create');
@@ -36,4 +43,19 @@ Route::get('/categories/{slug}', 'CategoryController@show');
 Route::get('/categories/{category}/edit', 'CategoryController@edit');
 Route::delete('/products/{category}', 'CategoryController@destroy');
 Route::put('/categories/{category}', 'CategoryController@update');
+
+/* Attributes */
+
+Route::get('/attributes', 'AttributeController@index');
+Route::get('/attributes/create', 'AttributeController@create');
+Route::post('/attributes/store', 'AttributeController@store');
+Route::get('/attributes/{attribute}/edit', 'AttributeController@edit');
+Route::put('/attributes/{attribute}', 'AttributeController@update');
+
+/* Attribute relations */
+Route::get('/attribute/relations', 'AttributeRelationController@index');
+Route::get('/attribute/relations/create', 'AttributeRelationController@create');
+Route::post('/attribute/relations/store', 'AttributeRelationController@store');
+Route::get('/attribute/relations/{attribute}/edit', 'AttributeRelationController@edit');
+Route::put('/attribute/relations/{attribute}', 'AttributeRelationController@update');
 
